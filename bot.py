110426b26193ff3675531a329532f6c530d928fd
FrankLaptopAri <frankuzcategui22@gmail.com>
2026-04-28 11:49:55 -0400

    Error linea 26 cookies

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ app = Client(
 
 # Instancias de servicios globales
 db = DatabaseManager()
-engine = MusicDownloader(Config.DOWNLOAD_DIR, Config.cookies.txt)
+engine = MusicDownloader(Config.DOWNLOAD_DIR, "cookies.txt")
 searcher = MusicSearcher() 
 
 # Almacenamiento temporal de resultados de búsqueda por usuario
